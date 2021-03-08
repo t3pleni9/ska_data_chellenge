5dd41099376b55b75304975e2831f3f0b954b43b
Arun <arunkumarmv1997@gmail.com>
2021-03-08 17:27:23 +0530

    Read boundry values from FITS file

--- src/skavengers/pipeline_setup.py
+++ src/skavengers/pipeline_setup.py
@@ -17,9 +17,9 @@ def substr_search(input_list, substr):
             return i
     return -1
 
-def gen_par_files(template_file, output_db_name):
+def gen_par_files(input_filename, template_file, output_db_name):
 
-# Read settings from configuration file
+    # Read settings from configuration file
     config = configparser.ConfigParser();
     success = config.read("pipeline_setup.ini");
 
@@ -32,21 +32,52 @@ def gen_par_files(template_file, output_db_name):
     number_of_slices = int(config["region"]["number_of_slices"])
     overlap_spec     = int(config["region"]["overlap_spec"])
 
+    # Open FITS file
+    try:
+        hdu = fits.open(input_filename);
+    except:
+        sys.stderr.write("Error: Failed to open FITS file: {}\n".format(input_filename));
+        sys.exit(1);
 
-#TODO Read from FITS, Dummy values for testing
+    # Extract header information
+    header = hdu[0].header;
 
-    x_min = 0
-    y_min = 0
-    z_min = 0
-    x_max = 500
-    y_max = 500
-    z_max = 500
+    bitpix = int(header["BITPIX"]);
+    word_size = int(abs(bitpix) / 8);
+    if(bitpix > 0):
+        wordsize = 4; # Assume 32-bit for integer arrays
+        sys.stderr.write("Warning: Data cube is of integer type; assuming 32 bit per pixel.\n");
+
+    naxis = int(header["NAXIS"]);
+    if(naxis < 3 or naxis > 4):
+        sys.stderr.write("Error: Data cube is not three-dimensional.\n");
+        hdu.close();
+        sys.exit(1);
+
+    nx = int(header["NAXIS1"]);
+    ny = int(header["NAXIS2"]);
+    nz = int(header["NAXIS3"]);
+
+    if(nz == 1 and naxis == 4):
+        nz = int(header["NAXIS4"]);
+        sys.stderr.write("Warning: Swapping 3rd and 4th axis of 4D cube.\n");
+
+    # Close FITS file
+    hdu.close();
+
+    # Entire FITS cube
+    x_min = 0;
+    y_min = 0;
+    z_min = 0;
+    x_max = nx - 1;
+    y_max = ny - 1;
+    z_max = nz - 1;
 
 #TODO Add RAM and region size calculations
 
     slice_size_z = int(math.floor(float(z_max - z_min + 1 +(number_of_slices - 1)*overlap_spec) / float(number_of_slices)))
 
-# Read template parameter file
+    # Read template parameter file
     try:
         with open(template_file) as par_file:
             template_par = par_file.readlines()
@@ -55,8 +86,7 @@ def gen_par_files(template_file, output_db_name):
         sys.exit(16)
 
 
-# Create set of parameter files
-
+    # Create set of parameter files
     for z in range(number_of_slices):
 
         par = template_par[:]
@@ -76,6 +106,8 @@ def gen_par_files(template_file, output_db_name):
         if(i < 0): par.append("output.filename  =  {0}_{1:03d}\n".format(output_db_name, z));
         else: par[i] = "output.filename  =  {0}_{1:03d}\n".format(output_db_name, z);
 
+#TODO Write parameter files into specific directory
+#
         # Dump parameters into new file
         filename = "sofia_{0:03d}.par".format(z);
         sys.stdout.write("  {0}\n".format(filename));
